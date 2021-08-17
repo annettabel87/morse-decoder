b1f7b5c8f976333de750d8bfdb8c28ea83ebbbb1
annettabel87 <annettabel@rambler.ru>
2021-08-17 16:31:22 +0500

    feat: add code

--- src/index.js
+++ src/index.js
@@ -39,6 +39,37 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arrLetter = [];
+    let str = '';
+    let count = 0;
+
+    for (let char of expr) {
+        str += char;
+        count++;
+        if (count === 10) {
+            arrLetter.push(str)
+            str = '';
+            count = 0;
+        }
+    }
+
+    return arrLetter.map(item => {
+        let letter = '';
+        if (item === '**********') {
+            letter = ' ';
+            return letter;
+        }
+        for (let i = 0; i <= item.length; i = i + 2) {
+            if (item[i] === '1' && item[i + 1] === '1') letter += '-';
+            if (item[i] === '1' && item[i + 1] === '0') letter += '.';
+        }
+        return letter;
+    }).map(letter => {
+        if (letter === ' ') return ' ';
+        if (letter in MORSE_TABLE) {
+            return MORSE_TABLE[letter]
+        }
+    }).join('');
 }
 
 module.exports = {
